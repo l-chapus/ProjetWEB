73da9b4e4affe67157208f4c9ba2a72f510f7453
l-chapus <louka.chapus@gmail.com>
2023-04-29 21:20:33 +0200

    ajout de l'affichage des messages d'erreurs pour l'écran de connexion, tout les cas sont pris en compte. La page Index est presque terminer

---
+++ API/js/gestion_utilisateur.js
@@ -0,0 +1,105 @@
+function creation(){
+    const utilisateur_existant = document.getElementById("email_creation_existant");
+    const utilisateur_incomplet = document.getElementById("email_creation_incomplet");
+    const utilisateur_creer = document.getElementById("email_creation_creer");
+    const email = document.getElementById("email_creation");
+    const password = document.getElementById("password_creation");
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../API/php/creer_utilisateur.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    xhr.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        console.log("Réponse du script PHP : " + xhr.responseText);
+
+        if(xhr.responseText === "formulaire_incomplet"){
+            utilisateur_incomplet.classList.replace('reset', 'afficher');
+            utilisateur_existant.classList.replace('afficher', 'reset');
+            utilisateur_creer.classList.replace('succes', 'reset');
+        }
+        if(xhr.responseText === "existe"){
+            utilisateur_existant.classList.replace('reset', 'afficher');
+            utilisateur_creer.classList.replace('succes', 'reset');
+            utilisateur_incomplet.classList.replace('afficher', 'reset');
+        }
+        if(xhr.responseText === "existe_pas"){
+            utilisateur_existant.classList.replace('afficher', 'reset');
+            utilisateur_incomplet.classList.replace('afficher', 'reset');
+            utilisateur_creer.classList.replace('reset', 'succes');
+        }
+    }
+    else if(xhr.status !== 200) {
+        console.log("Erreur : " + xhr.statusText);
+    }
+    };
+
+    xhr.send("email="+ email.value+"&password="+password.value);
+}
+
+function reset(){
+    document.getElementById("email_connexion_manquant").classList.replace('afficher', 'reset');
+    document.getElementById("email_connexion_inconnu").classList.replace('afficher', 'reset');
+    document.getElementById("password_connexion_manquant").classList.replace('afficher', 'reset');
+    document.getElementById("password_connexion_incorrect").classList.replace('afficher', 'reset');
+    document.getElementById("univers_manquant").classList.replace('afficher', 'reset');
+}
+
+
+function connexion(){
+    console.log("test");
+    const email_connexion_manquant = document.getElementById("email_connexion_manquant");
+    const email_connexion_inconnu = document.getElementById("email_connexion_inconnu");
+    const password_connexion_manquant = document.getElementById("password_connexion_manquant");
+    const password_connexion_incorrect = document.getElementById("password_connexion_incorrect");
+    const univers_manquant = document.getElementById("univers_manquant");
+    
+    const email = document.getElementById("email_connexion");
+    const password = document.getElementById("password_connexion");
+    const univers = document.getElementById("univers_select");
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../API/php/connexion.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    xhr.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        console.log("Réponse du script PHP : " + xhr.responseText);
+        console.log(xhr);
+        if(xhr.responseURL === 'http://localhost:3000/front/galaxie.php')
+        {
+            window.location.href = "../front/galaxie.php";
+        }
+        if(xhr.responseText === "password_manquant"){
+           reset();
+           password_connexion_manquant.classList.replace('reset', 'afficher');
+        }
+        if(xhr.responseText === "passsword_incorrect"){
+           reset();
+           password_connexion_incorrect.classList.replace('reset', 'afficher');
+        }
+        if(xhr.responseText === "email_manquant"){
+            reset();
+            email_connexion_manquant.classList.replace('reset', 'afficher');
+        }
+        if(xhr.responseText === "password_manquant_email_manquant"){
+            reset();
+            email_connexion_manquant.classList.replace('reset', 'afficher');
+            password_connexion_manquant.classList.replace('reset', 'afficher');
+        }
+        if(xhr.responseText === "utilisateur_inconnu"){
+            reset();
+            email_connexion_inconnu.classList.replace('reset', 'afficher');
+        }
+        if(xhr.responseText === "univers_manquant"){
+            reset();
+            univers_manquant.classList.replace('reset', 'afficher');
+        }
+    }
+    else if(xhr.status !== 200) {
+        console.log("Erreur : " + xhr.statusText);
+    }
+    };
+
+    xhr.send("email="+ email.value+"&password="+password.value+"&univers="+univers.value);
+}
--- API/php/connexion.php
+++ API/php/connexion.php
@@ -5,56 +5,71 @@ include 'pdo.php';
 // Vérifier si le formulaire a été soumis
 if (isset($_POST['email']) && isset($_POST['password'])) 
 {
-
-  // Récupérer les informations d'identification du formulaire
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $univers = $_POST['univers'];
-
-  $email = $_POST['email'];
-  $stmt = $db->prepare("SELECT * FROM utilisateurs WHERE email = :email");
-  $stmt->bindValue(":email",$email);
-  
-  // Exécution de la requète
-  if($stmt->execute())
+  if ($_POST['email']!='' && $_POST['password']==='' ) 
   {
-    if($stmt->rowCount() == 1)
-    {
+    echo 'password_manquant';
+  }
+  if ($_POST['email']==='' && $_POST['password']!='' ) 
+  {
+    echo 'email_manquant';
+  }
+  if ($_POST['email']==='' && $_POST['password']==='' ) 
+  {
+    echo 'password_manquant_email_manquant';
+  }
+  
+  if ($_POST['email']!='' && $_POST['password']!='' ) 
+  {
+    // Récupérer les informations d'identification du formulaire
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $univers = $_POST['univers'];
 
-    // Récupération de l'utilisateur
-    $utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    // Vérification du mot de passe avec la fonction password_verify()
-      if(password_verify($password, $utilisateur['password']))
+    $email = $_POST['email'];
+    $stmt = $db->prepare("SELECT * FROM utilisateurs WHERE email = :email");
+    $stmt->bindValue(":email",$email);
+  
+    // Exécution de la requète
+    if($stmt->execute())
+    {
+      if($stmt->rowCount() == 1)
       {
-      // Identifiants valides
-        echo "Identifiants valides.";
+
+      // Récupération de l'utilisateur
+      $utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        // Vérification du mot de passe avec la fonction password_verify()
+        if(password_verify($password, $utilisateur['password']))
+        {
+          // Identifiants valides
         
-        if($univers != ''){
-          // Démare la session
-          session_start();
-          $_SESSION['email'] = $email;
-          $_SESSION['loggedin'] = true;
-          $_SESSION['univers'] = $univers;
-          var_dump($_SESSION);
+          if($univers != ''){
+            // Démare la session
+            session_start();
+            $_SESSION['email'] = $email;
+            $_SESSION['loggedin'] = true;
+            $_SESSION['univers'] = $univers;
+            //var_dump($_SESSION);
         
-          // Rediriger l'utilisateur vers la page d'accueil
-          header("Location:../../front/galaxie.php");
+            // Rediriger l'utilisateur vers la page d'accueil
+            header("Location:../../front/galaxie.php");
+          }
+          else
+          {
+            echo "univers_manquant";
+          }
         }
-        else{
-          echo " Veuillez choisir un univers";
+        else
+        {
+        // Mot de passe incorrect
+        echo "passsword_incorrect";
         }
       }
       else
       {
-      // Mot de passe incorrect
-      echo "Mot de passe incorrect.";
+      // Utilisateur non trouvé
+        echo "utilisateur_inconnu";
       }
-    }
-    else
-    {
-    // Utilisateur non trouvé
-      echo "Utilisateur non trouvé.";
     }
   }
 }
